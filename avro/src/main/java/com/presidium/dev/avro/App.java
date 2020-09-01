fd0b09bbaaaf8e6ae573287c845e45dddf35ed76
prezydium <presidium.dev@gmail.com>
2020-09-01 17:42:58 +0200

    Initial commit

---
+++ avro/src/main/java/com/presidium/dev/avro/App.java
@@ -0,0 +1,10 @@
+package com.presidium.dev.avro;
+
+import org.apache.avro.Schema;
+
+public class App {
+
+    public static void main(String[] args) {
+        Schema.Parser parser = new Schema.Parser();
+    }
+}

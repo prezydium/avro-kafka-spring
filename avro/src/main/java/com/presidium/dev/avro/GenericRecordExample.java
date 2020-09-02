04782bc3c6986f5c8af16091bc5e6cb78541d56d
prezydium <presidium.dev@gmail.com>
2020-09-02 09:31:21 +0200

    GenericRecord example writing/reading to file

---
+++ avro/src/main/java/com/presidium/dev/avro/GenericRecordExample.java
@@ -0,0 +1,59 @@
+package com.presidium.dev.avro;
+
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.*;
+import org.apache.avro.io.DatumWriter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericRecordExample {
+
+    private static final String SCHEMA_FILEPATH = "C:\\dev\\avro-kafka-spring\\avro\\src\\main\\resources\\basic-monster.avsc";
+    private static final String FILE_NAME = "monster.avro";
+
+    public static void main(String[] args) throws IOException {
+        Schema.Parser parser = new Schema.Parser();
+        String schemaAsString = Files.readString(Path.of(SCHEMA_FILEPATH));
+        Schema schema = parser.parse(schemaAsString);
+        GenericRecordBuilder recordBuilder = new GenericRecordBuilder(schema);
+
+        recordBuilder.set("name", "Goblin");
+        recordBuilder.set("attack", 2.3f);
+        recordBuilder.set("health_points", 15);
+        recordBuilder.set("armor", 2);
+        recordBuilder.set("loot", 3);
+
+        GenericData.Record monster = recordBuilder.build();
+
+        System.out.println(monster);
+        writeAvroFile(monster);
+
+        System.out.println("Read from file:");
+        System.out.println(readAvroFile());
+    }
+
+    private static List<GenericRecord> readAvroFile() throws IOException {
+        List<GenericRecord> records = new ArrayList<>();
+        DataFileReader<GenericRecord> dataFileReader = new DataFileReader<>(new File(FILE_NAME), new GenericDatumReader<>());
+        while (dataFileReader.hasNext()) {
+            records.add(dataFileReader.next());
+        }
+        dataFileReader.close();
+        return records;
+    }
+
+    private static void writeAvroFile(GenericData.Record record) throws IOException {
+        DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(record.getSchema());
+        DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(datumWriter);
+        dataFileWriter.create(record.getSchema(), new File(FILE_NAME));
+        dataFileWriter.append(record);
+        dataFileWriter.close();
+    }
+}
